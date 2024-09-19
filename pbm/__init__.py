52e81d07b81144e755135ccaf3cdb056a378d0d8
enȷı <pixilreal@gmail.com>
2024-09-19 15:14:21 +0200

    v1.2

--- pbm/__init__.py
+++ pbm/__init__.py
@@ -1,8 +1,18 @@
-import os, logging, shutil, sys
+import sys, os, shutil, logging
+
+def choice_map(message: str, *choices: str, explicit_case: bool = False) -> str:
+    while True:
+        inp: str = input(f"\n\n\n{message}{"".join(" " * 80)}:").strip()
+
+        inp = inp if explicit_case else inp.lower()
+
+        if inp in choices:
+            return inp
+
+def confirmation() -> bool:
+    return choice_map("[y]es (confirm) | [n]o (cancel)", "y", "n") == "y"
 
 from random import randint
-
-from .utils import confirmation, choice_map
 
 logging.basicConfig(
     level=logging.INFO,
@@ -31,14 +41,15 @@ setup(name="pypbm repo",
 """
 
 class PBM:
-    latest_version = version
+    latest_version = "v1.2"
 
     @staticmethod
     def init(path: str | None = None) -> None:
         try:
             path = path or sys.argv[2]
         except IndexError:
-            logger.fatal("missing pbm repo path, use `python -m pbm init <path>` instead, or `python -m pbm init .` to initialize the current directory.")
+            logger.fatal(
+                "missing pbm repo path, use `python -m pbm init <path>` instead, or `python -m pbm init .` to initialize the current directory.")
             return
 
         if os.path.exists(".pbm"):
@@ -64,9 +75,10 @@ class PBM:
 
     @staticmethod
     def destroy() -> bool:
-        logger.warning("this will also detonate all builds of this pbm repo. consider using `python -m pbm base export . *` to back up your builds.")
+        logger.warning(
+            "this will also detonate all builds of this pbm repo. consider using `python -m pbm base export . *` to back up your builds.")
         logger.warning("are you sure you want to continue?")
-        
+
         if os.path.exists(".pbm"):
             if confirmation():
                 shutil.rmtree(".pbm")
@@ -160,7 +172,8 @@ class PBM:
             export_id: int = randint(0, 99999)
 
             os.mkdir(f".pbm/global_export_{export_id}")
-            shutil.make_archive(f"{location.strip("/\\")}/export_g{export_id}", "zip", f".pbm/global_export_{export_id}")
+            shutil.make_archive(f"{location.strip("/\\")}/export_g{export_id}", "zip",
+                                f".pbm/global_export_{export_id}")
             shutil.copytree(f".pbm/builds", f"{location.strip("/\\")}/g{export_id}.pbm")
             shutil.rmtree(f".pbm/global_export_{export_id}")
 
@@ -171,8 +184,9 @@ class PBM:
             PBM.build.new_base(base)
 
             if os.path.exists(".pbm"):
-                logger.warning("this will also detonate all builds of this pbm repo (and replace them with the imported ones). consider creating a new pbm repo in a separate directory, and merging manually.")
-                
+                logger.warning(
+                    "this will also detonate all builds of this pbm repo (and replace them with the imported ones). consider creating a new pbm repo in a separate directory, and merging manually.")
+
                 if confirmation():
                     location = location or "."
 
@@ -180,30 +194,30 @@ class PBM:
 
                     if not os.path.exists(source_path):
                         logger.error(f"import failed: {source_path} does not exist.")
-                        
+
                         return
 
                     if export_id.startswith("g"):
                         target_path = ".pbm/builds"
-                        
+
                         if os.path.exists(target_path):
                             shutil.rmtree(target_path)
-                            
+
                         shutil.copytree(source_path, target_path)
                         logger.info(f"Successfully imported global export '{export_id}'")
-                        
+
                     else:
                         target_path = f".pbm/builds/{base}"
-                        
+
                         if os.path.exists(target_path):
                             shutil.rmtree(target_path)
-                            
+
                         shutil.copytree(source_path, target_path)
                         logger.info(f"successfully imported base '{base}' from export '{export_id}'")
-                        
+
                 else:
                     logger.warning("cancelled base import")
-            
+
             else:
                 logger.warning("not a pbm repo, no .pbm directory.")
 
@@ -211,7 +225,8 @@ class PBM:
         def delete_base(base: str | None = None) -> None:
             base = base or "main"
 
-            logger.warning(f"this will delete your '{base}' build. consider using `python -m pbm export . {base}` to back up this build.")
+            logger.warning(
+                f"this will delete your '{base}' build. consider using `python -m pbm export . {base}` to back up this build.")
             logger.warning("are you sure you want to continue?")
 
             if confirmation():
